c5dec4d739f2475dd0e1fdbddc8c66393c0f677f
cheog0 <https://github.com/cheog0>
2025-07-26 12:01:13 +0900

    refactor: 로그인 응답 데이터 구조 타입 변경

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -3,8 +3,9 @@ import { apiClient } from '@/lib/apiClient';
 export const authApi = {
   login: (credentials: { email: string; password: string }) => {
     return apiClient.post<{
+      email: string;
+      name: string;
       authToken: string;
-      user: { id: number; email: string; name: string };
     }>('/api/login', credentials);
   },
 };
