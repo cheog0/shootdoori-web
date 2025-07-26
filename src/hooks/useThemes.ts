ed13663958169e86e8680342b51173fa3e8c9cd9
cheog0 <https://github.com/cheog0>
2025-07-26 11:57:34 +0900

    remove: 사용하지 않는 파일 삭제

--- src/hooks/useThemes.ts
+++
@@ -1,6 +0,0 @@
-import { useThemesQuery } from './queries';
-
-export function useThemes() {
-  const { data: themes, isLoading: loading, error } = useThemesQuery();
-  return { themes: themes || [], loading, error };
-}
