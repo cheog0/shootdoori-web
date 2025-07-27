4117a36f891f606bd38ad95a0e117e5227a3889d
cheog0 <https://github.com/cheog0>
2025-07-27 11:53:02 +0900

    chore: 불필요한 주석 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,64 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import MainPage from '@/pages/MainPage';
-import LoginPage from '@/pages/LoginPage';
-import NotFoundPage from '@/pages/NotFoundPage';
-import MyPage from '@/pages/MyPage';
-import GiftOrderPage from '@/pages/GiftOrderPage';
-import ThemePage from '@/pages/ThemePage';
-import { ProtectedRoute } from '@/components/shared/ui';
+import { Suspense, lazy } from 'react';
+import {
+  ProtectedRoute,
+  ErrorBoundary,
+  SuspenseFallback,
+} from '@/components/shared/ui';
 import { ROUTES } from '@/constants/routes';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import ProductDetailPage from './pages/ProductDetailPage';
+
+const MainPage = lazy(() => import('@/pages/MainPage'));
+const LoginPage = lazy(() => import('@/pages/LoginPage'));
+const NotFoundPage = lazy(() => import('@/pages/NotFoundPage'));
+const MyPage = lazy(() => import('@/pages/MyPage'));
+const GiftOrderPage = lazy(() => import('@/pages/GiftOrderPage'));
+const ThemePage = lazy(() => import('@/pages/ThemePage'));
+const ProductDetailPage = lazy(() => import('@/pages/ProductDetailPage'));
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path={ROUTES.HOME} element={<MainPage />} />
-        <Route path={ROUTES.LOGIN} element={<LoginPage />} />
-        <Route path={ROUTES.NOT_FOUND} element={<NotFoundPage />} />
-        <Route
-          path={ROUTES.MY_PAGE}
-          element={
-            <ProtectedRoute>
-              <MyPage />
-            </ProtectedRoute>
-          }
+      <ErrorBoundary>
+        <Suspense fallback={<SuspenseFallback />}>
+          <Routes>
+            <Route path={ROUTES.HOME} element={<MainPage />} />
+            <Route path={ROUTES.LOGIN} element={<LoginPage />} />
+            <Route path={ROUTES.NOT_FOUND} element={<NotFoundPage />} />
+            <Route
+              path={ROUTES.MY_PAGE}
+              element={
+                <ProtectedRoute>
+                  <MyPage />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path={ROUTES.ORDER_PAGE}
+              element={
+                <ProtectedRoute>
+                  <GiftOrderPage />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path={ROUTES.PRODUCT}
+              element={
+                <ProtectedRoute>
+                  <ProductDetailPage />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="/themes/:themeId" element={<ThemePage />} />
+          </Routes>
+        </Suspense>
+        <ToastContainer
+          position="top-center"
+          autoClose={2000}
+          hideProgressBar
         />
-        <Route
-          path={ROUTES.ORDER_PAGE}
-          element={
-            <ProtectedRoute>
-              <GiftOrderPage />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path={ROUTES.PRODUCT}
-          element={
-            <ProtectedRoute>
-              <ProductDetailPage />
-            </ProtectedRoute>
-          }
-        />
-        <Route path="/themes/:themeId" element={<ThemePage />} />
-      </Routes>
-      <ToastContainer position="top-center" autoClose={2000} hideProgressBar />
+      </ErrorBoundary>
     </Router>
   );
 }
